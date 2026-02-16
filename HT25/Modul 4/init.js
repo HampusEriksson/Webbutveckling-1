8c1717937eb2b489969a97c63996f63154119dfc
HampusEriksson <90573586+HampusEriksson@users.noreply.github.com>
2026-02-16 10:15:41 +0100

    l4

---
+++ HT25/Modul 4/init.js
@@ -0,0 +1,27 @@
+
+
+// document.querySelector("header") eller document.getElementsbytagname("header")[0]
+let header = document.querySelector("header").innerHTML = `
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+   
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Lektion 4 - init.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pokemon.html">POKEMON</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pricing.html">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>`
